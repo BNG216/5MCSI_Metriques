6688a8ac27efe99d7fd72b81edae5e965de7ea4d
BNG216 <91458488+BNG216@users.noreply.github.com>
2024-09-11 10:02:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,8 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def mapagecontact():
+    return render_template("contact.html")
 
 @app.route('/tawarano/')
 def meteo():
